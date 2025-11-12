57a4e7c0d4a788f72e5c355803f36af53261018d
PolPujolSantaella <pol.pujol.santaella@gmail.com>
2025-11-12 12:27:41 +0100

    First Check of the First Feed Forward

--- src/NeuralNet.py
+++ src/NeuralNet.py
@@ -47,38 +47,105 @@ class NeuralNet:
       "tanh": np.tanh
     }
 
+    # I fact is not in activation_functions raise Error
     if fact not in activation_functions:
       raise ValueError(f"Unknown activation fucntion '{fact}'. Must be one of: {list(activation_functions.keys())}")
     
-    self.fact_name = fact
+    self.fact_name = fact 
     self.fact = activation_functions[fact]
 
+    #Other input parameters
     self.epochs = epochs
     self.learning_rate = learning_rate
     self.momentum = momentum
     self.validation_split = validation_split
 
-# MAIN
+  def fit(self, X, y):
+    """
+    Train the neural network using backpropagation
+    Params:
+      X: np.ndarray
+        Training input data (n_samples x n_features)
+      y: np.ndarray
+        Target output data of shape (n_samples x output_size)
+    """
 
-layers = [4, 9, 5, 1]
-epochs = 100
-learning_rate = 0.01
-momentum = 0.9
-fact = "relu"
-validation_split = 0.2
-nn = NeuralNet(layers, epochs, learning_rate, momentum, fact, validation_split)
+    # STEP 1: Divide data into trainning and validation
+    val_size = int(X.shape[0] * self.validation_split)
+    X_val = X[:val_size]
+    y_val = y[:val_size]
+    X_train = X[val_size:]
+    y_train = y[val_size:]
+
+    X_val = X_val.to_numpy() if not isinstance(X_val, np.ndarray) else X_val
+    y_val = y_val.to_numpy() if not isinstance(y_val, np.ndarray) else y_val
+    X_train = X_train.to_numpy() if not isinstance(X_train, np.ndarray) else X_train
+    y_train = y_train.to_numpy() if not isinstance(y_train, np.ndarray) else y_train
+
+    # STEP 2: Initialize Weigths & Thresholds randomly
+    for lay in range(1, self.L):
+      # Random values from -1 to 1
+      self.w[lay] = np.random.uniform(-1, 1, (self.n[lay], self.n[lay - 1]))
+      self.theta[lay] = np.random.uniform(-1, 1, self.n[lay])
 
 
-print("L =", nn.L)
-print("n =", nn.n)
-print("\nxi =", nn.xi)
-print("xi[0] =", nn.xi[0])
-print("xi[1] =", nn.xi[1])
-print("\nw =", nn.w)
-print("w[1] =", nn.w[1])
-print("\nd_w =", nn.d_w)
-print("\nd_w_prev =", nn.d_w_prev)
-print("\ntheta =", nn.theta)
-print("\nd_theta =", nn.d_theta)
-print("\nd_theta_prev =", nn.d_theta_prev)
-print("\nActivation function:", nn.fact_name)
+    # STEP 3: For each epoch
+    for epoch in range(self.epochs):
+      # STEP 4: For each patter in trainning
+      for _ in range(X_train.shape[0]):
+
+        # STEP 5: Choose one random
+        random_pattern = np.random.randint(0, X_train.shape[0])
+        x_pattern = X_train[random_pattern]
+        y_pattern = y_train[random_pattern]
+
+        # STEP 6: Feed-Forward
+        self.xi[0] = x_pattern # The first input layer contains the random pattern
+
+        # For each layer
+        for l in range(1, self.L):
+          # First compute Sum of weights
+          # For each unit
+          for i in range(self.n[l]):
+            sum = 0.0
+            # For each unit of previous layer
+            for j in range(self.n[l - 1]):
+              # Sum = Weight Product * previous activation
+              sum += self.w[l][i][j] * self.xi[l - 1][j] 
+
+            # Compute field h(x)
+            self.h[l][i] = sum - self.theta[l][i] 
+
+            # Compute Activation (output of the unit)
+            self.xi[l][i] = self.fact(self.h[l][i])
+
+        # DEBUG FEED FORWARD
+        if epoch % 10 == 0 and _ == 0:
+          print(f"Epoch {epoch}: Feed-forward CHECKING")
+          print(f"Input 3 samples: {x_pattern[:3]}")
+          print(f"Input layer activation: {self.xi[0]}")
+          print(f"Output layer activation: {self.xi[self.L - 1]}")
+
+if __name__ == "__main__":
+  # MAIN
+  layers = [19, 32, 16, 1]
+  epochs = 100
+  learning_rate = 0.01
+  momentum = 0.9
+  fact = "relu"
+  validation_split = 0.2
+  nn = NeuralNet(layers, epochs, learning_rate, momentum, fact, validation_split)
+
+  print("L =", nn.L)
+  print("n =", nn.n)
+  print("\nxi =", nn.xi)
+  print("xi[0] =", nn.xi[0])
+  print("xi[1] =", nn.xi[1])
+  print("\nw =", nn.w)
+  print("w[1] =", nn.w[1])
+  print("\nd_w =", nn.d_w)
+  print("\nd_w_prev =", nn.d_w_prev)
+  print("\ntheta =", nn.theta)
+  print("\nd_theta =", nn.d_theta)
+  print("\nd_theta_prev =", nn.d_theta_prev)
+  print("\nActivation function:", nn.fact_name)
